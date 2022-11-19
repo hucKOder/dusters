491fbdec046849835e22617491a43881186a488c
Andrej Hucko <hucko.andrej.mail@gmail.com>
2022-11-19 12:24:08 +0100

    Working fog lights button
    Fog lights with battery spender

--- Assets/Scripts/CarLights.cs
+++ Assets/Scripts/CarLights.cs
@@ -8,59 +8,97 @@ public class CarLights : MonoBehaviour
     [Header("UI")]
     public Light leftFogLight;
     public Light rightFogLight;
-    public float battery;
+    public BatteryHandler battery;
 
-    public int USAGE = 10;
+    public int USAGE = 1;
     // In seconds
-    public int speedOfConsuption = 5;
+    public float speedOfConsuption = 1f;
     public int luminosity = 2;
 
     bool lightsOn = false;
 
-    Button thisButton;
+    public Button thisButton;
     public Sprite fogLightSprite;
     public Sprite fogLightSprite_disabled;
 
     private void Start()
     {
-        thisButton = GetComponent<Button>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (lightsOn)
-        {
-            StartCoroutine("SpendEnergy");
-        }
+
     }
 
-    IEnumerator SpendEnergy()
+    public bool CheckBattery()
     {
-        if (battery >= (battery - USAGE))
+        if ((battery.batteryCapacity - USAGE) < 0)
         {
-            battery -= USAGE;
-            yield return new WaitForSeconds(USAGE);
+            return false;
         }
         else
         {
-            turnOffLights();
+            return true;
         }
     }
 
-    public void turnOnLights()
+    void SpendEnergy()
+    {
+        if (CheckBattery())
+        {
+            battery.batteryCapacity -= USAGE;
+            Debug.Log("Draining battery with Fog lights!");
+            Debug.LogFormat("Batter capacity: {0}", battery.batteryCapacity);
+        }
+        else
+        {
+            if (lightsOn)
+            {
+                TurnOffLights();
+            }
+        }
+    }
+
+    public void SwitchLights()
+    {
+        if (lightsOn)
+        {
+            TurnOffLights();
+        }
+        else
+        {
+            if (CheckBattery())
+            {
+                TurnOnLights();
+            }
+            else
+            {
+                CannotUseAction();
+            }
+        }
+    }
+
+    void CannotUseAction()
+    {
+        Debug.Log("Not enough energy for this button!");
+    }
+
+    public void TurnOnLights()
     {
         thisButton.image.overrideSprite = fogLightSprite;
         leftFogLight.intensity = luminosity;
         rightFogLight.intensity = luminosity;
         lightsOn = true;
+        InvokeRepeating("SpendEnergy", 0f, speedOfConsuption);
     }
-
-    public void turnOffLights()
+    public void TurnOffLights()
     {
         thisButton.image.overrideSprite = fogLightSprite_disabled;
         leftFogLight.intensity = 0;
         rightFogLight.intensity = 0;
         lightsOn = false;
+        CancelInvoke("SpendEnergy");
     }
 }

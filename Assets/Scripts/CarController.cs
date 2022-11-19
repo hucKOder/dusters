1d379b0239a1410bea77f2c6e47cb35ed418c7ba
Martin Hoang <hoang.hog@gmail.com>
2022-11-19 01:26:14 +0100

    updated car controller, to be more linear

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,72 +5,27 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public WheelCollider frontLeftWheelCollider;
-    public WheelCollider frontRightWheelCollider;
-    public WheelCollider rearLeftWheelCollider;
-    public WheelCollider rearRightWheelCollider;
-    public Transform frontLeftWheelTransform;
-    public Transform frontRightWheelTransform;
-    public Transform rearLeftWheelTransform;
-    public Transform rearRightWheelTransform;
-    
-    public float maxSteeringAngle = 30f;
-    public float motorForce = 50f;
-    public float brakeForce = 0f;
-    
+    public float speed = 15;
+    public float turnSpeed = 60;
+
     private float _horizontalInput;
     private float _verticalInput;
-    private float _steerAngle;
-    private bool _isBreaking;
-    
+
     private void FixedUpdate()
     {
         GetInput();
-        HandleMotor();
-        HandleSteering();
-        UpdateWheels();
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * _verticalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * _horizontalInput);
     }
 
     private void GetInput()
     {
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
-        _isBreaking = Input.GetKey(KeyCode.Space);
-    }
-    
-    private void HandleMotor()
-    {
-        frontLeftWheelCollider.motorTorque = _verticalInput * motorForce;
-        frontRightWheelCollider.motorTorque = _verticalInput * motorForce;
-
-        brakeForce = _isBreaking ? 3000f : 0f;
-        frontLeftWheelCollider.brakeTorque = brakeForce;
-        frontRightWheelCollider.brakeTorque = brakeForce;
-        rearLeftWheelCollider.brakeTorque = brakeForce;
-        rearRightWheelCollider.brakeTorque = brakeForce;
-    }
-    
-    private void HandleSteering()
-    {
-        _steerAngle = maxSteeringAngle * _horizontalInput;
-        frontLeftWheelCollider.steerAngle = _steerAngle;
-        frontRightWheelCollider.steerAngle = _steerAngle;
-    }
-    
-    private void UpdateWheels()
-    {
-        UpdateWheelPos(frontLeftWheelCollider, frontLeftWheelTransform);
-        UpdateWheelPos(frontRightWheelCollider, frontRightWheelTransform);
-        UpdateWheelPos(rearLeftWheelCollider, rearLeftWheelTransform);
-        UpdateWheelPos(rearRightWheelCollider, rearRightWheelTransform);
-    }
-    
-    private void UpdateWheelPos(WheelCollider wheelCollider, Transform trans)
-    {
-        Vector3 pos;
-        Quaternion rot;
-        wheelCollider.GetWorldPose(out pos, out rot);
-        trans.rotation = rot;
-        trans.position = pos;
     }
 }
